cc392632f2b9ddec7687720d848a94c9c5907ff0
swilliams29 <swillia47@rockets.utoledo.edu>
2020-09-19 15:57:27 -0500

    initial check in

---
+++ TwoColumnListView/Models/Fruit.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Xamarin.Forms;
+
+namespace TwoColumnListView.Models
+{
+    public class Fruit
+    {
+        public string Name { get; set; }
+        public string Color { get; set; }
+
+        public string Image { get; set; }
+    }
+
+}
