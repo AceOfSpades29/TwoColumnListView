b8ebe0784bd858d07eab90cded32a15adbfca427
swilliams29 <swillia47@rockets.utoledo.edu>
2020-09-19 15:43:48 -0500

    test

---
+++ TwoColumnListView/Models/FruitContainer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoColumnListView.Models
+{
+    public class FruitContainer
+    {
+        public Fruit Fruit { get; set; }
+        public Fruit Fruit1 { get; set; }
+    }
+}
---
+++ TwoColumnListView/Models/FruitListGroup.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TwoColumnListView.Models
+{
+    public class FruitListGroup:List<FruitContainer>
+    {
+        public string FirstLetter { get; private set; }
+
+        public FruitListGroup(string firstLetter, List<FruitContainer> container) :base (container)
+        {
+            FirstLetter = firstLetter;
+        }
+    }
+}
